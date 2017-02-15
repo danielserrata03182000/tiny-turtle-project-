cb25aed65f25e297aff6c6dc7e85997abd0f179e
Daniel Serrata <daniel.s.student@lyonscommunityschool.org>
2017-02-15 15:11:41 +0000

    built house

---
+++ script.js
@@ -0,0 +1,15 @@
+ TinyTurtle.apply(window);
+ 
+ right(180);
+ forward(50);
+ left(90);
+ forward(50);
+ left(90);
+ forward(50);
+ left(90);
+ forward(50);
+ right(120);
+ forward(45);
+ right(115);
+ forward(47);
+ 
